bf071dbcefd9419b51843fbb4e4787494f855f62
atarbabgei <atarbabgei@gmail.com>
2024-07-09 21:50:53 +0100

    modified to accept external parameters

--- launch/client.launch.py
+++ launch/client.launch.py
@@ -1,13 +1,43 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    hostname = '192.168.0.100'
-    buffer_size = 256
-    topic_namespace = 'vicon'
+    # Declare the launch arguments
+    declare_server_arg = DeclareLaunchArgument(
+        'server', default_value='192.168.0.100',
+        description='Server address for the Vicon system'
+    )
+    
+    declare_buffer_size_arg = DeclareLaunchArgument(
+        'buffer_size', default_value='256',
+        description='Buffer size for the Vicon client'
+    )
+    
+    declare_topic_namespace_arg = DeclareLaunchArgument(
+        'namespace', default_value='mocap',
+        description='Namespace for the Vicon topics'
+    )
 
-    return LaunchDescription([Node(
-            package='mocap_vicon_client', executable='vicon_client', output='screen',
-            parameters=[{'hostname': hostname, 'buffer_size': buffer_size, 'namespace': topic_namespace}]
-        )])
+    # Use the launch arguments
+    server = LaunchConfiguration('server')
+    buffer_size = LaunchConfiguration('buffer_size')
+    topic_namespace = LaunchConfiguration('namespace')
+
+    return LaunchDescription([
+        declare_server_arg,
+        declare_buffer_size_arg,
+        declare_topic_namespace_arg,
+        Node(
+            package='mocap_vicon_client', 
+            executable='vicon_client', 
+            output='screen',
+            parameters=[{
+                'server': server, 
+                'buffer_size': buffer_size, 
+                'namespace': topic_namespace
+            }]
+        )
+    ])
